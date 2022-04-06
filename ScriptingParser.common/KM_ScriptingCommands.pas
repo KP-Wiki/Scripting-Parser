c6a9e3041a3c30dedbf85ec7ac6294200f9dd2d2
kromster80 <kromster80@gmail.com>
2022-04-06 14:15:40 +0300

    Cleanup

--- ScriptingParser.common/KM_ScriptingCommands.pas
+++ ScriptingParser.common/KM_ScriptingCommands.pas
@@ -56,42 +56,6 @@ uses
   KM_ScriptingConsts;
 
 
-// string functions
-// These function are replacements for string functions introduced after XE2 (XE5 probably)
-// Names are the same as in new Delphi versions, but with 'Str' prefix
-// We keep them here to support pre-XE5 compilation
-function StrSubstring(const aStr: string; aFrom: Integer): string;
-begin
-  Result := Copy(aStr, aFrom + 1, Length(aStr));
-end;
-
-
-function StrLastIndexOf(const aStr, aSubStr: string): Integer;
-var
-  I: Integer;
-begin
-  Result := -1;
-  for I := 1 to Length(aStr) do
-    if AnsiPos(aSubStr, StrSubstring(aStr, I-1)) <> 0 then
-      Result := I - 1;
-end;
-
-
-function StrTrimRightSeparators(const aStr: string): string;
-var
-  I, K: Integer;
-begin
-  K := 0;
-  for I := Length(aStr) downto 1 do
-  if not (aStr[I] in [',', ':', ';']) then
-  begin
-    K := I;
-    Break;
-  end;
-  Result := Copy(aStr, 1, K);
-end;
-
-
 { TKMCommandInfo }
 constructor TKMCommandInfo.Create;
 begin
